28b1810bd1b38106bda0e75eb03df7e5d4f78592
Maximilian Steuck <max@fount.io>
2021-11-23 14:18:34 +0100

    added vlidations to comparison, conditional validations still outstanding

--- app/models/comparison.rb
+++ app/models/comparison.rb
@@ -1,4 +1,11 @@
 class Comparison < ApplicationRecord
   belongs_to :user
   has_many :entries, dependent: :destroy
+  validates :topic, presence: true
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  # validates :country_one, if: (:publisher_one.nil? && :publisher_two.nil?), presence: true
+  # validates :country_two, if: (:publisher_one.nil? && :publisher_two.nil?), presence: true
+  # validates :publisher_one, if: (:country_one.nil? && :country_two.nil?), presence: true
+  # validates :publisher_two, if: (:country_one.nil? && :country_two.nil?), presence: true
 end
