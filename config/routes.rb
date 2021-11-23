e27f4f4271e4197a18fad2c71fecbf616d9f908b
Maximilian Steuck <max@fount.io>
2021-11-23 16:13:45 +0100

    first version of the api with fixed params and an basic index view to see the articles

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :comparisons, only: %i[show new create destroy]
+  resources :articles, only: [:index]
   resource :board, only: [:show]
   resources :entries, only: [:destroy]
 end
