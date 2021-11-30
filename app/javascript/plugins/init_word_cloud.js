98af27fb88834baeb6a6482a3939f1eb314cc5a0
Andre Ferrer <andrerferrer@hotmail.com>
2021-11-30 17:40:34 +0100

    change how the init word cloud function works

--- app/javascript/plugins/init_word_cloud.js
+++ app/javascript/plugins/init_word_cloud.js
@@ -1,12 +1,9 @@
 import WordCloud from 'wordcloud'
 
-const initWordCloud = () => {
-  const canvas = document.getElementById('my_canvas');
-  if (canvas) {
-    const list = JSON.parse(canvas.dataset.words)
-    console.log(list)
-    WordCloud(canvas, { list: list, fontWeight: '600', weightFactor: '20', color: 'random-dark', rotationSteps: '2', fontSize: '5'});
-  }
+const initWordCloud = (canvas) => {
+  const list = JSON.parse(canvas.dataset.words)
+  console.log(list)
+  WordCloud(canvas, { list: list, fontWeight: '600', weightFactor: '20', color: 'random-dark', rotationSteps: '2', fontSize: '5'});
 }
 
 export { initWordCloud };
